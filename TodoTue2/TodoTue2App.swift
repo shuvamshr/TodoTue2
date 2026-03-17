8ee4d8ab61b5d1fa640da60b13f69f7027215f83
shuvamshr <shuvamshr@outlook.com>
2026-03-17 14:20:04 +1100

    Initial Commit

---
+++ TodoTue2/TodoTue2App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTue2App.swift
+//  TodoTue2
+//
+//  Created by Shuvam Shrestha on 17/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoTue2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
